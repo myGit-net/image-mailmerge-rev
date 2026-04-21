bd15d22a0f9e323a34dd0e288b1396700a01aa06
myGit-net <rudyteja@maitreyawira-ds.sch.id>
2026-04-22 04:18:11 +0700

    Add files via upload

---
+++ src/components/types/fieldTypes.ts
@@ -0,0 +1,35 @@
+import { QRCodeFieldData } from '../QRCodeField';
+import { PhotoFieldData } from '../PhotoField';
+
+// Text field interface
+export interface TextField {
+  type: 'text';
+  name: string;
+  x: number;
+  y: number;
+  fontSize: number;
+  fontFamily: string;
+  color: string;
+  demoText: string;
+  textAlign: 'left' | 'center' | 'right';
+}
+
+// Union type for all field types
+export type Field = TextField | QRCodeFieldData | PhotoFieldData;
+
+// CSV row interface
+export interface CSVRow {
+  [key: string]: string;
+}
+
+// Field mapping interface
+export interface FieldMapping {
+  fieldName: string;
+  csvColumn: string | null;
+}
+
+// File name mapping interface
+export interface FileNameMapping {
+  csvColumn: string | null;
+  includeNumbering: boolean;
+}
