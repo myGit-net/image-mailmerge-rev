9e52006a9ec5e54721227fdf12f5673102356c6c
myGit-net <rudyteja@maitreyawira-ds.sch.id>
2026-04-25 19:55:27 +0700

    Delete src/components directory

--- src/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { ImageUploadSection } from './ImageUploadSection';
-export { SpreadsheetSection } from './SpreadsheetSection';
-export { GenerateImagesSection } from './GenerateImagesSection';
-export { FieldTypeModal } from './FieldTypeModal';
-export { CanvasPreview } from './CanvasPreview';
-export { FieldDefinitionSection } from './FieldDefinitionSection';
-export { FieldMappingSection } from './FieldMappingSection';
